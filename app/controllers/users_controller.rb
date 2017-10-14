3cefc5a1e503bb682341ab986f67a72a9319afc0
Amanda Wehrman <arwehrman@gmail.com>
2017-10-14 13:34:58 -0400

    added wine views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,55 @@
 class UsersController < ApplicationController
 
-  get "/signup" do
-  		erb :'users/signup'
-  	end
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
-  	post "/signup" do
-  		user = User.new(:username => params[:username], :password => params[:password])
-  		if user.save
-  			redirect 'users/login'
-  		else
-  			redirect '/failure'
-  		end
-  	end
-
-    get '/login' do
-      erb :'users/login'
+  get '/signup' do
+    if !logged_in?
+    erb :'users/create_user'#, locals: {message: "Please sign up before you sign in"}
+    else
+    redirect to :show_wines
     end
+  end
 
-    
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      erb :show_wines
+    end
+  end
+
+get '/login' do
+if !logged_in?
+erb :'users/login'
+else
+redirect '/tweets'
+end
+end
+
+post '/login' do
+user = User.find_by(:username => params[:username])
+if user && user.authenticate(params[:password])
+session[:user_id] = user.id
+redirect "/tweets"
+else
+redirect to '/signup'
+end
+end
+
+get '/logout' do
+if logged_in?
+session.destroy
+redirect to '/login'
+else
+redirect to '/'
+end
+end
+end
+
 end
