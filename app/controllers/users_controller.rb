3d8781fccc507621feb3a02ca2b0757ccf7acd8c
Amanda Wehrman <arwehrman@gmail.com>
2017-10-14 13:19:22 -0400

    added views, add routes to User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  get "/signup" do
+  		erb :'users/signup'
+  	end
+
+  	post "/signup" do
+  		user = User.new(:username => params[:username], :password => params[:password])
+  		if user.save
+  			redirect 'users/login'
+  		else
+  			redirect '/failure'
+  		end
+  	end
+
+    get '/login' do
+      erb :'users/login'
+    end
+    
 end
