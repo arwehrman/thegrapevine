e33ea781618d3720c05b9cc028350008bb6b0c55
Amanda Wehrman <arwehrman@gmail.com>
2017-10-14 22:45:02 -0400

    added flash to html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-
+  use Rack::Flash
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
 		set :session_secret, "wine_secret"
-    use Rack::Flash
+
   end
 
   get "/" do
