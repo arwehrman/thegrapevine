fcae2099ed65770dd23d057e2de8b8a157bfbb4b
Amanda Wehrman <arwehrman@gmail.com>
2017-10-23 15:58:57 -0400

    changed session name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-		set :session_secret, "wine_secret"
+		set :session_secret, "winesecret"
 
   end
 
