39f4eb346dcddcf99ebb8e15ca3f9d1da299dc1e
Amanda Wehrman <arwehrman@gmail.com>
2017-10-25 15:36:08 -0400

    updated controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   post '/wines/:wine_id/comments' do
     @wine = Wine.find_by(id: params[:wine_id])
-    @comment = Comment.new params[:content]
-    @comment.wine_id = @wine.id
+    @comment = Comment.new(:content => params[:content])
+    @comment.wine_id = @wine
     @comment.save
+    redirect back
   end
 
   delete '/wines/:wine_id/comments/:id' do
